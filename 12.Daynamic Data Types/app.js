5530fedc4cbce744930f39485588c44bb45c5d3c
Maham Babar <mahambabar11@gmail.com>
2022-10-14 04:50:50 -0700

    js practice

---
+++ 12.Daynamic Data Types/app.js
@@ -0,0 +1,6 @@
+let age = 20;
+console.log(age);
+age = "Maham";
+console.log(age);
+age = true;
+console.log(age);
---
+++ 13.Type of Operator/app.js
@@ -0,0 +1,9 @@
+let age =20;
+let a = "Hello";
+let isFeePaid = false;
+
+console.log (typeof age);
+console.log (typeof name);
+console.log (typeof isFeePaid);
+isFeePaid = 20;
+console.log(typeof isFeePaid);
