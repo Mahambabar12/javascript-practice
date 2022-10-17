7a043e36f4969ff6247f77f3b57bd358b845be78
Maham Babar <mahambabar11@gmail.com>
2022-10-17 03:46:09 -0700

    js practice

---
+++ 24.String Conversion Example/app.js
@@ -0,0 +1,13 @@
+// parseInt
+let a = 6;
+let b = "5";
+let c = parseInt(b);
+let d = a + c;
+console.log(d);
+
+// parseFloat
+let e = 6;
+let f = "5.5";
+let g = parseFloat(b);
+let h = e + f;
+console.log(h);
